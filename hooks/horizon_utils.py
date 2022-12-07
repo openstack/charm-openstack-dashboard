b429287b6ee3219e43c6026d957b4fdea393fbd7
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-07 23:10:55 +0000

    Adding tarfile member sanitization to extractall()

--- hooks/horizon_utils.py
+++ hooks/horizon_utils.py
@@ -903,7 +903,26 @@ def check_custom_theme():
     log('Retrieved resource: {}'.format(theme_file))
     if theme_file:
         with tarfile.open(theme_file, 'r:gz') as in_file:
-            in_file.extractall(CUSTOM_THEME_DIR)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(in_file, CUSTOM_THEME_DIR)
     custom_settings = '{}/local_settings.py'.format(CUSTOM_THEME_DIR)
     if os.path.isfile(custom_settings):
         try:
