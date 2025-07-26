9b4bde51421cc0ed23895c39c97bb7d1c52f5f0f
Jorge Merlino <jorge.merlino@canonical.com>
2025-07-26 17:58:07 -0300

    Add support for Content-Security-Policy header
    Adding a configuration parameter csp-options that, when set, adds a
    Content-Security-Policy header to the apache configuration.
    This header can prevent or minimize the risk of certain types of
    security threats by placing restrictions on the things the web page's
    code can do.
    
    Closes-Bug: #2118835
    
    Change-Id: I06f0b1c2787fa56460e5a196d3ca07c0a85c14e3
    Signed-off-by: Jorge Merlino <jorge.merlino@canonical.com>
    (cherry picked from commit 41250d97d114321469cfc2ae3cb086379f5e04ad)

--- hooks/horizon_contexts.py
+++ hooks/horizon_contexts.py
@@ -317,6 +317,7 @@ class ApacheContext(OSContextGenerator):
             'enforce_ssl': False,
             'hsts_max_age_seconds': config('hsts-max-age-seconds'),
             "custom_theme": config('custom-theme'),
+            'csp_options': config('csp-options'),
         }
 
         if config('enforce-ssl'):
